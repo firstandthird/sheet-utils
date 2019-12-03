73f98b1c9a3a6744aa11ed032d28d2d4bf4ec444
Eric Willis <eric@ecwillis.com>
2019-12-03 09:05:49 +0000

    Adding scheduler for general scheduling.

--- utils.js
+++ utils.js
@@ -64,3 +64,104 @@ function cellsToTable(cells) {
 
   return valArr;
 }
+
+function showScheduler() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var triggers = ScriptApp.getUserTriggers(ss);
+  
+  if (triggers.length) {
+    Logger.log(triggers[0].getUniqueId()); 
+  }
+  
+  var template = HtmlService.createTemplateFromFile('Index');
+  template.hasTriggers = (triggers.length > 0);
+  if (template.hasTriggers) {
+    var trigger = triggers[0];
+    var source = trigger.getTriggerSource();
+  }
+  var html = template.evaluate()
+    .setWidth(700);
+  
+  SpreadsheetApp.getUi()
+    .showModalDialog(html, 'Dialog title');
+}
+
+
+function updateSchedule(data) {
+  var upData = {};
+  data.forEach(function(v){
+    upData[v.name] = v.value;
+  });
+  // Delete the triggers by default.
+  var triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+  var weekDay = null;
+  switch(upData.dayOfWeek) {
+    case '0':
+      weekDay = ScriptApp.WeekDay.MONDAY;
+      break;
+    case '1':
+      weekDay = ScriptApp.WeekDay.TUESDAY;
+      break;
+    case '2':
+      weekDay = ScriptApp.WeekDay.WEDNESDAY;
+      break;
+    case '3':
+      weekDay = ScriptApp.WeekDay.THURSDAY;
+      break;
+    case '4':
+      weekDay = ScriptApp.WeekDay.FRIDAY;
+      break;
+    case '5':
+      weekDay = ScriptApp.WeekDay.SATURDAY;
+      break;
+    case '6':
+      weekDay = ScriptApp.WeekDay.SUNDAY;
+      break;
+  }
+  // Set up a new trigger?
+  if (upData.automate) {
+    switch (upData.interval) {
+      case 0:
+      case '0':
+        // Every Hour
+        ScriptApp.newTrigger('fetchAll')
+          .timeBased()
+          .everyHours(1)
+          .create();
+        break;
+      case 1:
+      case '1':
+        // Every Day
+        ScriptApp.newTrigger('fetchAll')
+          .timeBased()
+          .everyDays(1)
+          .atHour(upData.hourOfDay)
+          .create();
+        break;
+      case 2:
+      case '2':
+        // Every Week
+        ScriptApp.newTrigger('fetchAll')
+          .timeBased()
+          .everyWeeks(1)
+          .onWeekDay(weekDay)
+          .atHour(upData.hourOfDay)
+          .create();
+        break;
+      case 3:
+      case '3':
+        // Every Month
+        ScriptApp.newTrigger('fetchAll')
+          .timeBased()
+          .onMonthDay(upData.dayOfMonth)
+          .atHour(upData.hourOfDay)
+          .create();
+        break;
+      default:
+        Logger.log('Invalid interval');
+    }
+  }
+}
